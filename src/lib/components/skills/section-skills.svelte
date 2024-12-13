2eeaaf91b6bed6db779681705b1e59f0c8dcb43e
Enzo <digiovannienzo1@gmail.com>
2024-12-13 16:45:34 +0100

    style: new stacks

--- src/lib/components/skills/section-skills.svelte
+++ src/lib/components/skills/section-skills.svelte
@@ -1,7 +1,7 @@
 <section class="skills" id="competence">
   <h2>Mes Compétences !</h2>
-  <div class="web">
-    <p>WEB</p>
+  <div class="front-end">
+    <p>FRONT - END</p>
     <div class="skillsContainer">
       <div class="skill">
         <i class="fa-brands fa-html5"></i>
@@ -15,10 +15,22 @@
         <i class="fa-brands fa-square-js"></i>
         <span>JAVASCRIPT</span>
       </div>
+      
+    </div>
+  </div>
+  <div class="back-end">
+    <p>BACK - END</p>
+    <div class="skillsContainer">
+      <div class="skill">
+        <i class="fa-brands fa-golang"></i>
+        <span>GO</span>
+      </div>
       <div class="skill">
         <i class="fa-brands fa-php"></i>
+
         <span>PHP</span>
       </div>
+      
     </div>
   </div>
   <div class="mobile">
@@ -65,7 +77,7 @@
       grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
       place-items: start;
     }
-    .web {
+    .front-end {
       .skill {
         display: flex;
         align-items: center;
@@ -75,8 +87,7 @@
         &:nth-child(1) {
           i {
             color: #e65000;
-            background-color: rgb(230, 80, 0, 0.3);
-            border: 1px solid #e65000;
+          
           }
           span {
             color: #e65000;
@@ -86,8 +97,7 @@
         &:nth-child(2) {
           i {
             color: #ec407a;
-            background-color: rgb(236, 64, 122, 0.3);
-            border: 1px solid #ec407a;
+           
           }
           span {
             color: #ec407a;
@@ -96,26 +106,45 @@
         &:nth-child(3) {
           i {
             color: #efd81b;
-            background-color: rgb(239, 216, 27, 0.3);
-            border: 1px solid #efd81b;
+          
           }
           span {
             color: #efd81b;
           }
         }
-        &:nth-child(4) {
+        
+      }
+    }
+    .back-end {
+      .skill {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        gap: 20px;
+
+        &:nth-child(1) {
+          i {
+            color: #00AED8;
+
+
+          }
+          span {
+            color: #00AED8;
+          }
+        }
+        &:nth-child(2) {
           i {
             color: #7a86b8;
-            background-color: rgb(122, 134, 184, 0.3);
-            border: 1px solid #7a86b8;
           }
           span {
             color: #7a86b8;
           }
         }
-      }
     }
-    .mobile {
+
+    
+  }
+  .mobile {
       .skill {
         display: flex;
         align-items: center;
@@ -124,8 +153,7 @@
         &:nth-child(1) {
           i {
             color: #5dc7f8;
-            background-color: rgb(93, 199, 248, 0.3);
-            border: 1px solid #5dc7f8;
+         
           }
           span {
             color: #5dc7f8;
@@ -133,5 +161,5 @@
         }
       }
     }
-  }
+}
 </style>
